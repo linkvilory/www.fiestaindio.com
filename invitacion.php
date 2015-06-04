b1153827336cf0eb5ff91626c96c329b8f2e73a7
Jonathan <linkvilory@hotmail.com>
2015-06-04 16:56:34 -0500

    Inicio Proyecto
    Estructura finalizada, aun faltan las imágenes y estilo de la pagina

---
+++ invitacion.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+if(isset($_SESSION["codigoSrc"])){
+	$codigo = $_SESSION["codigoSrc"];
+
+	function sendEmail(){
+
+
+
+		return;
+	}
+
+}else{ $codigo = "XXXXXX"; }
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+
+	<!-- Meta -->
+	<meta charset="UTF-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Css -->
+	<link type="text/css" rel="stylesheet" href="css/font-awesome.css"/>
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.css"/>
+	<link type="text/css" rel="stylesheet" href="css/main.css"/>
+	
+	<!-- Icons -->
+
+	<!-- Title Web Site -->
+	<title>Una fiesta en un millón</title>
+	</head>
+	<body>
+
+	<!-- Boostrap Template -->
+	<section id="body" class="body">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<img class="logo" src="img/logo.png" alt="Logo Indio"/>
+				</div>
+			</div>
+			<div class="row">	
+				<div class="col-md-12">
+					<img src="img/titleForm.png" class="header" alt="titleForm"/>
+					<h2 class="text confirm">Confirmación de asistencia</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">	
+				<?php
+
+				if($_SESSION["codigoSrc"] == "lleno"){
+
+				?>
+					<p class="text">Lo sentimos, se ha llenado el cupo para el evento.</p>
+					<p class="text">Te invitamos a seguir al pendiente de nuestros próximos eventos.</p>
+				<?php	
+
+				}else{
+
+				?>
+					<pre class="text pre">
+Con este código de asistencia <b class="codigo"><?php echo $codigo; ?></b>
+serás parte de la historia.
+No olvides presentarlo el día del evento,
+será tu llave de acceso.
+
+27 de Junio
+De 11:00 a 23:00 hrs
+Hotel flor de mayo
+Matamoros 49, 62000
+Cuernavaca, Morélos, Mor.
+</pre>
+				<?php
+				}
+				?>
+				</div>
+			</div>
+		</div>
+
+	</section>
+	
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div clasS="col-md-12">
+					<img class="footerImg" src="img/footer.png" alt="Footer"/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<span class="copyright">Cuauhtemoc Moctezuma 2015. Todos los derechos reservados.</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">		
+					<span class="aviso">
+						<a class="ref" href="http://www.cuamoc.com/es/content/aviso-de-privacidad" target="_blank">Aviso de privacidad</a>
+					</span>
+					<span class="terminos">
+						<a class="ref" href="http://www.cuamoc.com/es/content/terminos-y-condiciones-de-uso-del-sitio-web-de-cuauhtemoc-moctezuma" target="_blank">Terminos y condiciones</a>
+					</span>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Javascript Init -->
+	<script type="text/javascript" src="js/jquery-1.11.2.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+
+	</body>
+</html>
+<?php
+
+//session_destroy();
+
+?>
---
+++ validation.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$email = $_POST["email"];
+$telefono = $_POST["telefono"];
+$validado = 0;
+
+if($nombre == "" || $apellidos == "" || $email == "" || $telefono == ""){
+
+	exit(0);
+
+}
+
+date_default_timezone_set('America/Mexico_City');
+$fecha = date("Y-m-d H:i:s");
+$codigo = getRandomAlphaNumeric();
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$databasename = "indioPilsner";
+$tablenameInv = "invitaciones";
+$tablenameReg = "registrados";
+
+$handle = mysql_connect($servername, $username, $password);
+$found = mysql_select_db($databasename, $handle);
+
+if ($found) {
+
+	$queryRegistrados = "SELECT * FROM `". $databasename ."`.`". $tablenameReg ."` WHERE `correo` = '". $email ."' ";
+	$resultadoRegistrados = mysql_query($queryRegistrados);
+	if(mysql_num_rows($resultadoRegistrados) == 1){
+		// El usuario si estaba previamente registrado y se le dará el acceso pertinente
+		$validado = 1;
+	}
+
+	$queryValidation = "SELECT * FROM `". $databasename ."`.`". $tablenameInv ."` WHERE `email` = '". $email ."' ";
+	$resultadoValidation = mysql_query($queryValidation);
+	if(mysql_num_rows($resultadoValidation) == 1){
+		// El usuario ya se habia registrado anteriormente
+		exit(0);
+	}
+
+	$query = "INSERT INTO `". $databasename ."`.`". $tablenameInv ."` (`id`, `nombre`, `apellido`, `email`, `telefono`, `fecha`, `codigo`, `validado`) VALUES (NULL, '". $nombre ."', '". $apellidos ."', '". $email ."', '". $telefono ."', '". $fecha ."', '". $codigo ."', '". $validado ."')";
+	$resultado = mysql_query($query);
+
+	if($validado == 1){
+		$_SESSION["codigoSrc"] = $codigo;
+	}else{
+		$_SESSION["codigoSrc"] = "lleno";
+	}
+	
+	echo 1;
+
+}
+
+/*
+ * Ingresar los datos y validar con la base de datos si el usuario es de los invitados
+ */
+
+function getRandomAlphaNumeric(){
+
+	$characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	$string = '';
+	for ($i = 0; $i < 8; $i++) {
+		$string .= $characters[rand(0, strlen($characters) - 1)];
+	}
+
+	return $string;
+}
+?>
